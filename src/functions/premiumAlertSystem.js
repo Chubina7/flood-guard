e9eecf3f11950d0043c636d088de0d9d9ae6c8ea
Luka Chubinidze <chubinidzel08@gmail.com>
2024-01-29 17:12:24 +0400

    finish alert system

--- src/functions/premiumAlertSystem.js
+++ src/functions/premiumAlertSystem.js
@@ -3,19 +3,32 @@ import alertSender from "./alertSender";
 let prevHumidity
 let prevTemperature
 let message
+let type
 
 const premiumAlertSystem = (rawData) => {
     let rawHumidity = rawData.Humidity
     let rawTemperature = rawData.Temperature
 
-    if (rawHumidity !== prevHumidity || rawTemperature !== prevTemperature) {
-        if (rawHumidity == 22 && rawTemperature == 23) {
-            message = "ერთი ქეისის ტესტი"
-        }
+    if (rawHumidity >= 100 && rawHumidity !== prevHumidity) {
+        message = "მიიღე ზომები! სტიქიური მოვლენა გარდაუვალია! დატოვე ადგილი!"
+        type = "warn"
         prevHumidity = rawHumidity
-        prevTemperature = rawTemperature
-        return alertSender(message, "info")
+    } else if (rawHumidity >= 90 && rawHumidity !== prevHumidity) {
+        message = "მობილიზდი! მდგომარეობა უარესდება!"
+        type = "info"
+        prevHumidity = rawHumidity
+    } else if (rawHumidity >= 60 && rawHumidity !== prevHumidity) {
+        message = "ყურადღება! უხვი ნალექის რაოდენობამ შესაძლოა წყალდიდობის საფრთხე შექმნას"
+        type = "info"
+        prevHumidity = rawHumidity
+    } else if (rawHumidity === prevHumidity) { // სატესტო ნოტიფიკაცია; რომ უფრო თვალსაჩინო იყოს კოდის მუშაობის სისტემატიურობა
+        message = `ტენიანობა არის ${rawHumidity}mm, ტემპერატურა კი ${rawTemperature}°`
+        type = "info"
+        prevHumidity = rawHumidity
     }
+
+    return alertSender(message, type)
+
 }
 
 export default premiumAlertSystem
